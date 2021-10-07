0f8549e39967aa2b52ee90dc9a556d2cd65e0fe7
PavaniKempula <pavanikempula1998@gmail.com>
2021-10-08 02:09:18 +0530

    ADDED : WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM

---
+++ src/main/java/com/blz/empwage/EmployeeWageBuilder.java
@@ -0,0 +1,8 @@
+package com.blz.empwage;
+
+public class EmployeeWageBuilder {
+    public static void main(String[] args) {
+        // Displaying Welcome Message
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}
