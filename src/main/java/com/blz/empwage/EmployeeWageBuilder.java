7b0c34eb83f932130268b4aadef0af93402adc2d
PavaniKempula <pavanikempula1998@gmail.com>
2021-10-11 06:06:38 +0530

    ADDED : Used Class method with function parameters for calculating employee wage for multiple companies

--- src/main/java/com/blz/empwage/EmployeeWageBuilder.java
+++ src/main/java/com/blz/empwage/EmployeeWageBuilder.java
@@ -1,27 +1,22 @@
 package com.blz.empwage;
 
-/**
- * @author Pavani
- *  With respect to employees attendance,
- *  Calculating employee daily and monthly wage.
- */
+import java.lang.*;
 
 public class EmployeeWageBuilder {
 
     private static final int IS_PART_TIME = 1;
     private static final int IS_FULL_TIME = 2;
-    private static final int WAGE_PER_HR = 20;
-    private static final int NUM_OF_WORKING_DAYS = 20;
-    private static final int MAX_HRS_IN_MONTH = 100;
 
-    private static void computeEmpWage() {
+    public static void computEmpWage( ){
+
         int empHrs = 0;
         int empWage = 0;
         int totalEmpWage = 0;
         int totalEmpHrs = 0;
         int totalWorkingDays = 0;
 
-        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        while(totalEmpHrs<=Company.maxHrsPerMonth && totalWorkingDays<Company.numOfWorkinDays) {
+
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
@@ -36,25 +31,25 @@ public class EmployeeWageBuilder {
                     break;
             }
             totalEmpHrs += empHrs;
-            System.out.println("Day :" + totalWorkingDays + "Emp Hrs : " + empHrs);
+            System.out.println("Day : " + totalWorkingDays + " Emp Hrs : " + empHrs);
 
         }
-        totalEmpWage = totalEmpHrs * WAGE_PER_HR;
-        System.out.println("Total Emp Hrs is:" + totalEmpHrs);
-        System.out.println("Total Emp Wage is :" + totalEmpWage);
+
+        totalEmpWage =totalEmpHrs *Company.wagePerHrs;
+        System.out.println("Total Emp Hrs is:"+totalEmpHrs);
+        System.out.println("Company is: "+Company.company +", Total Emp Wage is :"+totalEmpWage);
+        // return totalEmpWage;
+    }
+
+    public static void main(String[] args){
+        EmployeeWageBuilder c = new EmployeeWageBuilder();
+        Company cap = new Company("Capgemini",20,2,10);
+        c.computEmpWage();
+        System.out.println("__________________________________________");
+        Company dmart = new Company("Dmart",20,3,100);
+        c.computEmpWage();
 
     }
 
-    /*
-     * @param args
-     * Creating objects of a class
-     * Calling different methods with objects if not static
-     * Passing values while calling method.
-     */
-    public static void main(String args[]) {
-
-        computeEmpWage();
-
-    }
 
 }
